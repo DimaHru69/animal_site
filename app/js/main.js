d0654a55c1cf2c85e5c5a75b81a40e45a2f23c41
DimaHru69 <ches_2008@ukr.net>
2020-05-29 11:06:59 +0300

    start

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 1000,
+        easing: 'ease',
+        infinite: true,
+        initialSlide:0,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        centerMode: false,
+    });
+});
