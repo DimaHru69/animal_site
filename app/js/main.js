6581252f8b2981374bed16de59e21633cea74567
DimaHru69 <ches_2008@ukr.net>
2020-06-11 10:54:34 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.slider').slick({
         arrows: false,
-        dots: true,
+        dots: false,
         adaptiveHeight: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        speed: 1000,
+        speed: 1000, 
         easing: 'ease',
         infinite: true,
         initialSlide:0,
